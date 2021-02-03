092416fb1778f4ea0d916b37c60c9158c1e18c1c
machukhinktato <tarabrinmv@gmail.com>
2021-02-03 19:12:35 +0300

    in progress

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.title

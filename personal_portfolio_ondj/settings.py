413ffe9c9c35c0f843ce616042f8932be9cf2785
machukhinktato <tarabrinmv@gmail.com>
2021-02-04 18:18:56 +0300

    in progress

--- personal_portfolio_ondj/settings.py
+++ personal_portfolio_ondj/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'personal_portfolio_ondj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

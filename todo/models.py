821958d84804a10ffb33e5a376f08fcc3be49029
machukhinktato <tarabrinmv@gmail.com>
2021-02-06 15:43:27 +0300

    all works project in  progress

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo_Model(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(null=True)
+    high_priority = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)

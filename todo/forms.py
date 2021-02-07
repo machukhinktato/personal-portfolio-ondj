773a9926504a2f7a7ef0938fc741d59fce6c7903
machukhinktato <tarabrinmv@gmail.com>
2021-02-07 12:42:25 +0300

    all works project in  progress

--- todo/forms.py
+++ todo/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import ToDoModel
 
@@ -5,4 +6,12 @@ from .models import ToDoModel
 class ToDoForm(ModelForm):
     class Meta:
         model = ToDoModel
-        fields = ['title','description', 'high_priority']
+        fields = ['title', 'description', 'high_priority']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            # 'high_priority': forms.CheckboxInput(attrs={'class': 'form-control'}),
+        }
+
+    # def  clean_
--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_main, name='todo_url'),
-    path('/create', views.todo_create, name='todo_create_url')
+    path('create/', views.todo_create, name='todo_create_url')
 ]

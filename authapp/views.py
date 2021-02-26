e5e7bd75dab6cf00e6ae9cdf7a7a6b0795a96419
machukhinktato <tarabrinmv@gmail.com>
2021-02-26 18:37:40 +0300

    all works project in  progress

--- authapp/views.py
+++ authapp/views.py
@@ -11,9 +11,11 @@ def auth_login(request):
     fields = UserProfileLoginForm(data=request.POST or None)
     _next = request.GET['next'] if 'next' in request.GET.keys() else ''
     if request.method == 'POST' and fields.is_valid():
-        username = fields.cleaned_data['username']
-        password = fields.cleaned_data['password']
-        user = auth.authenticate(username=username, password=password)
+        # username = fields.cleaned_data['username']
+        # password = fields.cleaned_data['password']
+        user = auth.authenticate(
+            username=fields.cleaned_data['username'],
+            password=fields.cleaned_data['password'])
         if user.is_active:
             auth.login(request, user)
             if 'next' in request.POST.keys():
@@ -30,6 +32,4 @@ def auth_login(request):
 
 
 def auth_reg(request):
-    if request.method == 'GET':
-        fields = UserProfileRegisterForm
-        return render(request, 'authapp/registration.html', {'fields': fields})
+
